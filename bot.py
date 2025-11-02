7dc56f10defeb95790203af6fcfb49a4c77567bd
Timur Agleev <42980610+agleevtimur@users.noreply.github.com>
2025-11-02 11:57:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,8 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 from core.logger import setup_logger
 from core.rules import load_rules, save_rules
-from core.ozon_api import get_stocks
-from core.stocks import aggregate_by_cluster
-from core.decide import clusters_on_for_sku, clusters_for_group, discount_for_group
+from core.ozon_api import get_stocks, get_warehouses
+from core.stocks import aggregate_by_cluster, ensure_id_map
 from run_once import main as run_pipeline
 
 load_dotenv()
