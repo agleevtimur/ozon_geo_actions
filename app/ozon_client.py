7cd9d54fe096ae4b7e94cabfe9457e8ba5c51482
Timur Agleev <42980610+agleevtimur@users.noreply.github.com>
2025-11-07 01:08:39 +0300

    Update ozon_client.py

--- app/ozon_client.py
+++ app/ozon_client.py
@@ -13,9 +13,9 @@ HEADERS_BASE = {
     "Accept": "application/json, text/plain, */*",
     "Origin": "https://seller.ozon.ru",
     "Priority": "u=3, i",
-    "Sec-Fetch-Dest": "empty"
-    "Sec-Fetch-Mode": "cors"
-    "Sec-Fetch-Site": "same-origin"
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-origin",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.1 Safari/605.1.15"
 }
 
